525fedb1db9a0240d9bddd9eab27171be97b08cd
DalyChouikh <chouikhdaly215@gmail.com>
2024-10-27 22:57:50 +0100

    Added routes/router to setup the needed routes

---
+++ routes/router.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"database/sql"
+
+	"github.com/DalyChouikh/url-shortener/api"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(db *sql.DB) *gin.Engine {
+	urlHandler := api.NewUrlHandler(db)
+
+	router := gin.Default()
+
+	router.POST("/shorten", urlHandler.HandlePostUrl)
+	router.GET("/:short_code", urlHandler.HandleGetUrl)
+
+	return router
+
+}
