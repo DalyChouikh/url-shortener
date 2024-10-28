4b31dade78e2d9234d4485a409d60516fcea5b39
DalyChouikh <chouikhdaly215@gmail.com>
2024-10-28 17:55:17 +0100

    imported godotenv to load the correct env file depending on the environment

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/DalyChouikh/url-shortener/config"
 	"github.com/DalyChouikh/url-shortener/routes"
+	"github.com/joho/godotenv"
 )
 
+const (
+	DEVELOPMENT = "development"
+	PRODUCTION  = "production"
+)
+
+func init() {
+	env := os.Getenv("ENV")
+	if env == "" {
+		os.Setenv("ENV", "development")
+	}
+}
+
 func main() {
-	db := config.SetupDatabase()
+	env := os.Getenv("ENV")
+	if env == DEVELOPMENT {
+		godotenv.Load(".env.development")
+	}
+	if env == PRODUCTION {
+		godotenv.Load(".env.production")
+	}
+	databaseConnectionString := os.Getenv("DATABASE_CONNECTION_STRING")
+	log.Println("Database connection string: ", databaseConnectionString)
+	db := config.SetupDatabase(databaseConnectionString)
 	defer db.Close()
 
 	router := routes.SetupRoutes(db)
