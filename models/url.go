32290428406d3831c159660d49d950d0dddc8373
DalyChouikh <chouikhdaly215@gmail.com>
2024-10-28 17:51:18 +0100

    Updated SQL queries to match the postgres driver

--- models/url.go
+++ models/url.go
@@ -3,12 +3,12 @@ package models
 import "database/sql"
 
 func SaveUrl(db *sql.DB, longUrl, shortCode string) error {
-	_, err := db.Exec("INSERT INTO URL (long_url, short_code) VALUES (?, ?)", longUrl, shortCode)
+	_, err := db.Exec("INSERT INTO URL (long_url, short_code) VALUES ($1, $2)", longUrl, shortCode)
 	return err
 }
 
 func GetUrl(db *sql.DB, shortCode string) (string, error) {
 	var longUrl string
-	err := db.QueryRow("SELECT long_url FROM URL WHERE short_code = ?", shortCode).Scan(&longUrl)
+	err := db.QueryRow("SELECT long_url FROM URL WHERE short_code = $1", shortCode).Scan(&longUrl)
 	return longUrl, err
 }
