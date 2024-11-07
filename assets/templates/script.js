d684cad8161dacbcf7fe9c8afef5fcc3fe582541
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-07 16:14:23 +0100

    Added a div and img element where it will display the qrcode

--- assets/templates/script.js
+++ assets/templates/script.js
@@ -1,16 +1,13 @@
 async function generateShortLink() {
-  // Get the value of the input field
   const longUrl = document.getElementById("urlInput").value;
   const errorMessage = document.getElementById("errorMessage");
   const generatedLink = document.getElementById("generatedLink");
   const linkContainer = document.getElementById("linkContainer");
   const copyButton = document.getElementById("copyButton");
 
-  // Clear any previous messages or link
   errorMessage.style.display = "none";
   generatedLink.style.display = "none";
 
-  // Send a POST request to the backend
   try {
     const response = await fetch("/api/v1/shorten", {
       method: "POST",
@@ -20,18 +17,17 @@ async function generateShortLink() {
       body: JSON.stringify({ long_url: longUrl }),
     });
 
-    // Parse the response
     const data = await response.json();
 
     if (response.ok) {
-      // Display the shortened link
       generatedLink.href = data.short_url;
       generatedLink.textContent = data.short_url;
       generatedLink.style.display = "flex";
       linkContainer.style.visibility = "visible";
       copyButton.style.visibility = "visible";
+
+      generateCode(data.qrcode);
     } else {
-      // Display the error message
       errorMessage.textContent = data.error || "An error occurred";
       errorMessage.style.display = "block";
     }
@@ -41,10 +37,16 @@ async function generateShortLink() {
   }
 }
 
+function generateCode(qrcode) {
+  const qrCodeContainer = document.getElementById("qrCodeContainer");
+  const qrCodeNode = document.createElement("img");
+  qrCodeNode.src = `data:image/png;base64, ${qrcode}`;
+  qrCodeContainer.appendChild(qrCodeNode);
+}
+
 function copyLink() {
   const generatedLink = document.getElementById("generatedLink");
 
-  // Copy the URL from the generatedLink's href attribute
   navigator.clipboard
     .writeText(generatedLink.href)
     .then(() => {
