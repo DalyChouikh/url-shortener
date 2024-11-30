2dfd948bebd5f734f27479fb1a9a25b08553429b
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-30 22:52:46 +0100

    Refactor url model, added url repository that will interact with the database and added created_at and clicks fields to the URL model

--- models/url.go
+++ models/url.go
@@ -1,14 +1,53 @@
 package models
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
 
-func SaveUrl(db *sql.DB, longUrl, shortCode string) error {
-	_, err := db.Exec("INSERT INTO URL (long_url, short_code) VALUES ($1, $2)", longUrl, shortCode)
-	return err
+type URL struct {
+	ID        int64
+	LongURL   string
+	ShortCode string
+	CreatedAt time.Time
+	Clicks    int64
 }
 
-func GetUrl(db *sql.DB, shortCode string) (string, error) {
-	var longUrl string
-	err := db.QueryRow("SELECT long_url FROM URL WHERE short_code = $1", shortCode).Scan(&longUrl)
-	return longUrl, err
+type URLRepository struct {
+	db *sql.DB
+}
+
+func NewURLRepository(db *sql.DB) *URLRepository {
+	return &URLRepository{db: db}
+}
+
+func (r *URLRepository) Save(ctx context.Context, url *URL) error {
+	query := `
+	INSERT INTO URL (long_url, short_url, created_at)
+	VALUES($1, $2, $3)
+	RETURNING id`
+	err := r.db.QueryRowContext(ctx, query, url.LongURL, url.ShortCode, time.Now()).Scan(&url.ID)
+	if err != nil {
+		return fmt.Errorf("failed to save URL: %w", err)
+	}
+	return nil
+}
+
+func (r *URLRepository) GetByShortCode(ctx context.Context, shortCode string) (*URL, error) {
+	url := &URL{}
+	query := `
+	UPDATE URL
+	SET clicks = clicks + 1
+	WHERE short_code = $1
+	RETURNING id, long_url, short_code, created_at, clicks`
+	err := r.db.QueryRowContext(ctx, query, shortCode).Scan(&url.ID, &url.LongURL, &url.ShortCode, &url.CreatedAt, &url.Clicks)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("URL not found: %w", err)
+		}
+		return nil, fmt.Errorf("failed to get URL: %w", err)
+	}
+	return url, nil
 }
