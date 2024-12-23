ba69466f9cb8e08097aabb25bc99d1503ab20acc
DalyChouikh <chouikhdaly215@gmail.com>
2024-12-23 19:59:33 +0100

    Enhance AjaxRequired middleware to return JSON response for non-AJAX requests

--- middleware/ajax_required.go
+++ middleware/ajax_required.go
@@ -1,16 +1,19 @@
 package middleware
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func AjaxRequired() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        if c.GetHeader("X-Requested-With") != "XMLHttpRequest" {
-            c.AbortWithStatus(http.StatusNotFound)
-            return
-        }
-        c.Next()
-    }
+	return func(c *gin.Context) {
+		if c.GetHeader("X-Requested-With") != "XMLHttpRequest" {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error":   "not_found",
+				"message": "Page not found",
+			})
+			return
+		}
+		c.Next()
+	}
 }
